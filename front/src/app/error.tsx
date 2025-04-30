3fbbba3639cb473d76412a19a637625723ff02ac
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-04-30 15:22:17 +0200

    feat: :construction: Error Page Init

--- front/src/app/error.tsx
+++ front/src/app/error.tsx
@@ -1,1 +1,21 @@
-// Gestion d'erreur globale
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Une erreur est survenue</h1>
+    </div>
+  );
+}
