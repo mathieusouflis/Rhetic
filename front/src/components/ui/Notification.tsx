3a6fc463c23e1856cdcc0aedafa53055971c27d9
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-01 20:14:43 +0200

    style: :lipstick: Round + leading

--- front/src/components/ui/Notification.tsx
+++ front/src/components/ui/Notification.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Body } from "./Typography";
+import { Body, Small } from "./Typography";
 
 type NotificationVariant = "white" | "red";
 type NotificationStatus = "default" | "active";
@@ -16,15 +16,15 @@ const variantStyles: Record<
 > = {
   white: {
     default:
-      "bg-[var(--white-bg-transparent-active)] border border-[var(--white-border-transparent-active)] text-[var(--white)] rounded-[10px]",
+      "bg-[var(--white-bg-transparent-active)] border border-[var(--white-border-transparent-active)] text-[var(--white)] rounded-[6px]",
     active:
-      "bg-[var(--white-bg-transparent)] border border-[var(--white-border-transparent)] text-[var(--white-border-transparent)] rounded-[10px]",
+      "bg-[var(--white-bg-transparent)] border border-[var(--white-border-transparent)] text-[var(--white-border-transparent)] rounded-[6px]",
   },
   red: {
     default:
-      "bg-[var(--red-bg-transparent-active)] border border-[var(--red-border-transparent-active)] text-[var(--red-border-transparent-active)] rounded-[10px]",
+      "bg-[var(--red-bg-transparent-active)] border border-[var(--red-border-transparent-active)] text-[var(--red-border-transparent-active)] rounded-[6px]",
     active:
-      "bg-[var(--red-bg-transparent)] border border-[var(--red-border-transparent)] text-[var(--red-border-transparent-alt-plus)] rounded-[10px]",
+      "bg-[var(--red-bg-transparent)] border border-[var(--red-border-transparent)] text-[var(--red-border-transparent-alt-plus)] rounded-[6px]",
   },
 };
 
@@ -35,9 +35,9 @@ const Notification: React.FC<NotificationProps> = ({
 }) => {
   return (
     <div
-      className={`min-w-[31px] min-h-[31px] w-fit p-1 flex justify-center items-center ${variantStyles[color][status]}`}
+      className={`min-w-[22px] h-[22px] w-fit p-1 flex justify-center items-center ${variantStyles[color][status]}`}
     >
-      <Body>{children}</Body>
+      <Small className="leading-none">{children}</Small>
     </div>
   );
 };
