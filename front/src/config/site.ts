1657f65aa3015658daf6c3d87146adc9dece0eb9
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-02 13:01:52 +0200

    chore: :construction:

--- front/src/config/site.ts
+++ front/src/config/site.ts
@@ -1,1 +1,18 @@
-// Configuration du site
+// // Configuration du site
+// export type SiteConfig = {
+//   name: string;
+//   description: string;
+//   url: string;
+//   links: {
+//     github: string;
+//   };
+// };
+
+// export const siteConfig: SiteConfig = {
+//   name: "Rhetic",
+//   description: "Management system for Hetic students and teachers",
+//   url: "https://rhetic.com",
+//   links: {
+//     github: "https://github.com/rhetic/rhetic",
+//   },
+// };
