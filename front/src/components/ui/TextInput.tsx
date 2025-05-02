d149390dbd70ad81f0e62fcb7540912d8b87383f
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-02 12:50:23 +0200

    feat: :lipstick: TextInput Component

--- front/src/components/ui/TextInput.tsx
+++ front/src/components/ui/TextInput.tsx
@@ -3,19 +3,29 @@ import Icon, { IconName } from "./Icons";
 import { Link } from "./Link";
 import classNames from "classnames";
 
+type TextInputVariants = "black" | "fill";
 export interface TextInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   leftIcon?: boolean;
+  variant?: TextInputVariants;
   leftIconName?: IconName;
   linkText?: string;
   placeholder?: string;
 }
+
+const variantStyles: Record<TextInputVariants, string> = {
+  black: "",
+  fill: "border-[var(--black-500)] bg-[var(--black-700)] border rounded-[10px]",
+};
+
+const baseStyles = "flex flex-row items-center gap-2.5 w-full px-2.5 py-2";
 
 const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
   (
     {
       className = "",
       leftIcon = false,
+      variant = "fill",
       leftIconName = "home",
       disabled = false,
       placeholder,
@@ -26,22 +36,31 @@ const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
   ) => {
     const [isFocused, setIsFocused] = useState(false);
 
+    const inputClasses = classNames(
+      baseStyles,
+      variantStyles[variant],
+      className,
+      {
+        "border-[var(--yellow-border-transparent-active)]":
+          isFocused && variant === "fill",
+      }
+    );
+
     return (
       <div className="flex flex-col gap-1">
-        <div
-          className={classNames(
-            "flex flex-row items-center border-[var(--black-500)] bg-[var(--black-700)] gap-2.5 w-full border rounded-[10px] px-2.5 py-2",
-            {
-              "border-[var(--yellow-border-transparent-active)]": isFocused,
-            }
-          )}
-        >
+        <div className={inputClasses}>
           {leftIcon && (
             <Icon name={leftIconName} color="var(--yellow)" size={18} />
           )}
           <input
             ref={ref}
-            className="w-full text-[14px] outline-none bg-transparent placeholder:text-[var(--black-100)]"
+            className={classNames(
+              "w-full outline-none bg-transparent placeholder:text-[var(--black-100)]",
+              {
+                "text-[14px]": variant === "fill",
+                "text-[16px]": variant === "black",
+              }
+            )}
             disabled={disabled}
             placeholder={placeholder ? placeholder : "Enter text..."}
             {...props}
