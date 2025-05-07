00ac10bbf4212a70b3674f98b0f19e7152997dc8
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-07 11:16:20 +0200

    refactor: :lipstick: BigButton Variant styles changes

--- front/src/components/ui/BigButton.tsx
+++ front/src/components/ui/BigButton.tsx
@@ -1,8 +1,9 @@
 import { forwardRef } from "react";
 import Icon, { IconName } from "./Icons";
 import { Body } from "./Typography";
+import classNames from "classnames";
 
-type ButtonVariant = "black" | "white";
+type ButtonVariant = "black" | "white" | "conic";
 
 type ButtonSize = "default";
 
@@ -18,16 +19,25 @@ export interface ButtonProps
 }
 
 const variantStyles: Record<ButtonVariant, string> = {
-  black: "bg-[#232323] border border-[#323232] text-white rounded-[10px]",
-  white: "",
+  black:
+    "bg-[#232323] border border-[var(--black-400)] text-white rounded-[10px] px-2.5 py-3",
+  white:
+    "bg-[var(--white)] border border-transparent text-black rounded-[10px] px-2.5 py-3",
+  conic:
+    "border-t border-b border-[var(--black-500)] px-2.5 py-[21px] hover:bg-[var(--black-700)]",
+};
+
+const iconStyles: Record<ButtonVariant, string> = {
+  black: "var(--white)",
+  white: "var(--black-800)",
+  conic: "var(--white)",
 };
 
 const sizeStyles: Record<ButtonSize, string> = {
   default: "",
 };
 
-const baseStyles =
-  "flex flex-row justify-between items-center px-2.5 py-3 gap-2.5";
+const baseStyles = "flex flex-row items-center gap-2.5";
 
 const BigButton = forwardRef<HTMLButtonElement, ButtonProps>(
   (
@@ -46,12 +56,16 @@ const BigButton = forwardRef<HTMLButtonElement, ButtonProps>(
     },
     ref
   ) => {
-    const buttonClasses = [
+    const buttonClasses = classNames(
       baseStyles,
+      {
+        "justify-center": !rightIcon,
+        "justify-between": rightIcon,
+      },
       variantStyles[variant],
       sizeStyles[size],
-      className,
-    ].join(" ");
+      className
+    );
 
     return (
       <button
@@ -61,10 +75,14 @@ const BigButton = forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       >
         <div className="flex flex-row gap-2.5">
-          {leftIcon && <Icon name={leftIconName} size={18} />}
+          {leftIcon && (
+            <Icon name={leftIconName} color={iconStyles[variant]} size={18} />
+          )}
           <Body>{children}</Body>
         </div>
-        {rightIcon && <Icon name={rightIconName} size={18} />}
+        {rightIcon && (
+          <Icon name={rightIconName} color={iconStyles[variant]} size={18} />
+        )}
       </button>
     );
   }
