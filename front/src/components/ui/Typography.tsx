a2bd7f6a373ca5427b632e0e0aecf41aa865c279
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-07 11:18:18 +0200

    refactor: Style + Typo Changes

--- front/src/components/ui/Typography.tsx
+++ front/src/components/ui/Typography.tsx
@@ -4,14 +4,15 @@ const baseStyle = "";
 
 const typoStyles = {
   display: baseStyle + " text-[36px]",
-  h1: baseStyle + " text-[20px]",
-  h2: baseStyle + " text-[16px]",
+  h1: baseStyle + " text-[20px] font-bold",
+  h2: baseStyle + " text-[16px] font-semibold",
   big_body: baseStyle + " text-[16px]",
   body: baseStyle + " text-[14px]",
   small: baseStyle + " text-[13px]",
   tiny: baseStyle + " text-[11px]",
 };
-interface BaseTypographyProps {
+
+interface BaseTypographyProps extends React.HTMLAttributes<HTMLElement> {
   children: React.ReactNode;
   className?: string;
 }
@@ -19,34 +20,69 @@ interface BaseTypographyProps {
 export const Display: React.FC<BaseTypographyProps> = ({
   children,
   className = "",
-}) => <h1 className={`${typoStyles.display} ${className}`}>{children}</h1>;
+  ...props
+}) => (
+  <h1 className={`${typoStyles.display} ${className}`} {...props}>
+    {children}
+  </h1>
+);
 
 export const H1: React.FC<BaseTypographyProps> = ({
   children,
   className = "",
-}) => <h1 className={`${typoStyles.h1} ${className}`}>{children}</h1>;
+  ...props
+}) => (
+  <h1 className={`${typoStyles.h1} ${className}`} {...props}>
+    {children}
+  </h1>
+);
 
 export const H2: React.FC<BaseTypographyProps> = ({
   children,
   className = "",
-}) => <h2 className={`${typoStyles.h2} ${className}`}>{children}</h2>;
+  ...props
+}) => (
+  <h2 className={`${typoStyles.h2} ${className}`} {...props}>
+    {children}
+  </h2>
+);
 
 export const BigBody: React.FC<BaseTypographyProps> = ({
   children,
   className = "",
-}) => <p className={`${typoStyles.big_body} ${className}`}>{children}</p>;
+  ...props
+}) => (
+  <p className={`${typoStyles.big_body} ${className}`} {...props}>
+    {children}
+  </p>
+);
 
 export const Body: React.FC<BaseTypographyProps> = ({
   children,
   className = "",
-}) => <p className={`${typoStyles.body} ${className}`}>{children}</p>;
+  ...props
+}) => (
+  <p className={`${typoStyles.body} ${className}`} {...props}>
+    {children}
+  </p>
+);
 
 export const Small: React.FC<BaseTypographyProps> = ({
   children,
   className = "",
-}) => <p className={`${typoStyles.small} ${className}`}>{children}</p>;
+  ...props
+}) => (
+  <p className={`${typoStyles.small} ${className}`} {...props}>
+    {children}
+  </p>
+);
 
 export const Tiny: React.FC<BaseTypographyProps> = ({
   children,
   className = "",
-}) => <p className={`${typoStyles.tiny} ${className}`}>{children}</p>;
+  ...props
+}) => (
+  <p className={`${typoStyles.tiny} ${className}`} {...props}>
+    {children}
+  </p>
+);
