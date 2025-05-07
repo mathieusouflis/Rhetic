a9f6a4ff8f383041dd04e42b8a7c7f2cf3a01edd
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-07 11:21:24 +0200

    refactor: :wrench: Config Adition (Languages, Shortcuts, Tags)

---
+++ front/src/config/langues.ts
@@ -0,0 +1,8 @@
+export const LANGUAGES_LIST = [
+  { name: "French", tag: "FR_fr" },
+  { name: "Bambara", tag: "" },
+  { name: "Arabic", tag: "" },
+  { name: "Italian", tag: "" },
+  { name: "Creole", tag: "" },
+  { name: "Greek", tag: "GR_gr" },
+];
--- front/src/config/query.config.ts
+++ front/src/config/query.config.ts
@@ -1,15 +1,15 @@
-// import { QueryClientConfig } from "@tanstack/react-query";
+import { QueryClientConfig } from "@tanstack/react-query";
 
-// export const QUERY_CONFIG: QueryClientConfig = {
-//   defaultOptions: {
-//     queries: {
-//       retry: 1,
-//       staleTime: 5 * 60 * 1000, // 5 minutes
-//       gcTime: 10 * 60 * 1000, // 10 minutes
-//       refetchOnWindowFocus: false,
-//     },
-//     mutations: {
-//       retry: 1,
-//     },
-//   },
-// } as const;
+export const QUERY_CONFIG: QueryClientConfig = {
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+      refetchOnWindowFocus: false,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+} as const;
