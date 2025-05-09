49082c09d46db00ea79a6e0689e0d5b9e80ccd95
Kd3Us <jules.sorrentino@gmail.com>
2025-05-10 01:14:37 +0200

    feat: :sparkles: Add Delete Post with permission

---
+++ strapi/src/api/post/routes/post-delete.ts
@@ -0,0 +1,15 @@
+// src/api/post/routes/post-delete.ts
+
+export default {
+  routes: [
+    {
+      method: 'DELETE',
+      path: '/posts/:id',
+      handler: 'post.delete',
+      config: {
+        policies: ['api::post.delete-permission'],
+        middlewares: [],
+      },
+    },
+  ],
+};
