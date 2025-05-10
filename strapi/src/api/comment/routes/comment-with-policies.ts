34c58a56cb3ab1ae65ea3c74e8ff4193de824286
Kd3Us <jules.sorrentino@gmail.com>
2025-05-10 16:56:01 +0200

    fix: :ambulance: is_author fix

--- strapi/src/api/comment/routes/comment-with-policies.ts
+++ strapi/src/api/comment/routes/comment-with-policies.ts
@@ -1,22 +1,22 @@
 export default {
-    routes: [
-      {
-        method: 'PUT',
-        path: '/comments/:id',
-        handler: 'comment.update',
-        config: {
-          policies: ['api::comment.is-author'],
-          middlewares: [],
-        },
+  routes: [
+    {
+      method: 'PUT',
+      path: '/comments/:id',
+      handler: 'comment.update',
+      config: {
+        policies: ['api::comment.is-author'],
+        middlewares: [],
       },
-      {
-        method: 'DELETE',
-        path: '/comments/:id',
-        handler: 'comment.delete',
-        config: {
-          policies: ['api::comment.is-author', 'api::comment.is-post-moderator'],
-          middlewares: [],
-        },
+    },
+    {
+      method: 'DELETE',
+      path: '/comments/:id',
+      handler: 'comment.delete',
+      config: {
+        policies: ['api::comment.is-author', 'api::comment.is-post-moderator'],
+        middlewares: [],
       },
-    ],
-  };
+    },
+  ],
+};
---
+++ strapi/src/api/post/routes/index.ts
@@ -0,0 +1,6 @@
+module.exports = [
+  require('./post'),
+  require('./post-vote'),
+  require('./post-create-update'),
+  require('./post-delete'),
+];
