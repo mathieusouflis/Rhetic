5b01b19e43428a4903bb5d7450f6b288ade64ab3
Kd3Us <jules.sorrentino@gmail.com>
2025-05-11 00:00:24 +0200

    feat: :sparkles: Populate statistics

--- strapi/src/api/comment/routes/comment.ts
+++ strapi/src/api/comment/routes/comment.ts
@@ -1,7 +1,12 @@
-/**
- * comment router
- */
-
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::comment.comment');
+export default factories.createCoreRouter('api::comment.comment', {
+  config: {
+    find: {
+      middlewares: ['api::comment.populate-statistics']
+    },
+    findOne: {
+      middlewares: ['api::comment.populate-statistics']
+    }
+  }
+});
--- strapi/src/api/post/routes/post.ts
+++ strapi/src/api/post/routes/post.ts
@@ -1,7 +1,12 @@
-/**
- * post router
- */
-
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::post.post');
+export default factories.createCoreRouter('api::post.post', {
+  config: {
+    find: {
+      middlewares: ['api::post.populate-statistics']
+    },
+    findOne: {
+      middlewares: ['api::post.populate-statistics']
+    }
+  }
+});
