b92e60ad5ad255403dc5f4829d0ab337b888731b
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-13 22:51:37 +0200

    fix: :zap: Eslint Rule add + Notification page soon...

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -13,4 +13,6 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.rules["@typescript-eslint/no-explicit-any"] = "off";
+
 export default eslintConfig;
