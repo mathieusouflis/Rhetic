8f0153970ffbb215e2a1359e0d925e47b416ad8f
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-14 10:13:33 +0200

    Soon pages 2.0

--- front/src/app/(main)/saved/page.tsx
+++ front/src/app/(main)/saved/page.tsx
@@ -2,7 +2,7 @@
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Notifications</h1>
+      <h1 className="text-4xl font-bold">Saved</h1>
       <p className="mt-4 text-lg">Soon...</p>
     </div>
   );
