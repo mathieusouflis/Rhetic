d20421c733a90e1c75a285d51f525fe47e5fc90b
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-05-14 10:10:45 +0200

    Soon pages

--- front/src/app/(main)/messages/page.tsx
+++ front/src/app/(main)/messages/page.tsx
@@ -1,5 +1,9 @@
 "use client";
-
 export default function Page() {
-  return <></>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Messages</h1>
+      <p className="mt-4 text-lg">Soon...</p>
+    </div>
+  );
 }
